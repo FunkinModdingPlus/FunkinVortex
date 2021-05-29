092f52c0422168b2032d1248a4d3f7c367df7fbe
Bulby <Benharless820@gmail.com>
2021-05-28 20:45:15 -0400

    Initial commit

---
+++ source/PlayState.hx
@@ -0,0 +1,17 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
